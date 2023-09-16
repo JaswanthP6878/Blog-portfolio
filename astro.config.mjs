fdf086423dc91d637e8e89c155732616ecdbb6f8
JaswanthP6878 <klucse2000039034@gmail.com>
2023-09-16 20:29:45 +0530

    first deployment fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jaswanthp6878.github.io',
-    base: '/Blog-portfolio',
+    site: 'https://jaswanthp6878.github.io'
   })
