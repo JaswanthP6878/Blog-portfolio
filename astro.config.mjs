495462572164f7ada99b0b1ee5b98d96a7336370
JaswanthP6878 <klucse2000039034@gmail.com>
2023-09-24 10:38:52 +0530

    changed domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://JaswanthP6878.github.io',
+    site: 'https://jaswanthp.me',
   })
